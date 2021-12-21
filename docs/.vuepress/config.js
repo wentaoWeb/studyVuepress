082aa3bbd66147a0ae04a7b13c00b43ddd097dd8
朱文涛 <983418559@qq.com>
2021-12-21 14:37:48 +0800

    加上脚本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'TypeScript4 文档',
     description: 'TypeScript4 最新官方文档翻译',
+    base: '/studyVuepress/',
     theme: 'reco',
     themeConfig: {
         subSidebar: 'auto'
