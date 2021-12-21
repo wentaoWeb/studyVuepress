6edc52266dda859b1fbd603ca801973ee810be98
朱文涛 <983418559@qq.com>
2021-12-21 14:21:17 +0800

    第一次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    title: 'TypeScript4 文档',
+    description: 'TypeScript4 最新官方文档翻译',
+    theme: 'reco',
+    themeConfig: {
+        subSidebar: 'auto'
+    },
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '轻言丶的 JavaScript 博客',
+                items: [
+                    { text: 'CSDN', link: 'https://blog.csdn.net/zwt1995?type=sub&spm=1001.2100.3001.5348' }
+                ]
+            }
+        ],
+        sidebar: [
+            {
+                title: '欢迎学习',
+                path: '/',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "学前必读", path: "/" }
+                ]
+            },
+            {
+                title: "基础学习",
+                path: '/handbook/ConditionalTypes',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "条件类型", path: "/handbook/ConditionalTypes" },
+                    { title: "泛型", path: "/handbook/Generics" }
+                ],
+            }
+        ]
+    }
+
+}
